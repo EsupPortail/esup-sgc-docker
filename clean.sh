9ad844d82cb80bcd2b888cb91dc33dcceccb7dde
Vincent Bonamy <vbonamy@gmail.com>
2023-05-17 17:12:41 +0200

    esup-sgc-client ...

--- clean.sh
+++ clean.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # suppression fichiers compilés ...
-rm -rf esup-sgc esup-nfc-tag-server
-mkdir esup-sgc esup-nfc-tag-server 
+rm -rf esup-sgc esup-nfc-tag-server esup-sgc-client
+mkdir esup-sgc esup-nfc-tag-server esup-sgc-client
 
 # suppression de tout docker - confirmation est demandée à l'utilisateur - par défaut N
 docker system prune -a
