b1433c47dca48efdf86e1d881424a34b74c5c116
ctarade <ctarade@univ-lr.fr>
2023-09-08 15:39:14 +0200

    ne plus recompiler les projets si les binaires existent déjà dans les volumes

---
+++ config/esup-sgc-build/start-docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+DST_DIR_EMPTY=$([ "$(ls -A $DST_DIR)" ] && echo "False" || echo "True")
+
+if [ "$DST_DIR_EMPTY" = "True" ]
+then
+    echo "Répertoire $DST_DIR vide, récupération depuis $GIT_REPO_URL (tag $GIT_REPO_TAG)"
+    git clone $GIT_REPO_URL .
+    git checkout -b $GIT_REPO_TAG $(git describe --tags --abbrev=0)
+else
+    echo "Répertoire $DST_DIR non vide, conversation du binaire existant"
+fi
+
+echo "Montage des fichiers configuration"
+ln -sf /docker-config/applicationContext-services.xml $DST_DIR/src/main/resources/META-INF/spring/applicationContext-services.xml
+ln -sf /docker-config/database.properties $DST_DIR/src/main/resources/META-INF/spring/database.properties
+ln -sf /docker-config/persistence.xml $DST_DIR/src/main/resources/META-INF/persistence.xml
+
+if [ "$DST_DIR_EMPTY" = "True" ]
+then
+    echo "Compilation et installation"
+    mvn compile exec:java -Dexec.args="dbupgrade"
+    mvn package && rm -rf target/ROOT
+    mv target/$(git describe --tags --abbrev=0 | sed 's/esup-//') target/ROOT
+fi
+
+echo "Build terminé"
+tail -f /dev/null 
