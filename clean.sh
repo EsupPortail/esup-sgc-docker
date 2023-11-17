e53ea98c691282eab7ed23fb62c4efc86e6f3da1
ctarade <ctarade@univ-lr.fr>
2023-11-17 15:18:18 +0100

    fix clean

--- clean.sh
+++ clean.sh
@@ -4,5 +4,5 @@
 docker rm $(docker ps -a -f status=exited -q); docker rmi $(docker images -q) -f
 
 # suppression des volumes build
-docker volume remove sgc-docker_esup-nfc-tag-vol
-docker volume remove sgc-docker_esup-sgc-vol
+docker volume remove esup-sgc-docker_esup-nfc-tag-vol
+docker volume remove esup-sgc-docker_esup-sgc-vol
